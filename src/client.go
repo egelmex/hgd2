49d7663d39008727bf8b09057cd9f40e3b64e0c8
egelmex <ellism88@gmail.com>
2013-06-06 09:27:58 +0100

    gofmt'ing the code.

--- src/client.go
+++ src/client.go
@@ -1,18 +1,18 @@
 package main
 
-import ( "fmt"
-	"log"
+import (
 	"encoding/json"
-	"net/http"
-	"io/ioutil"
-	"os"
-	"lib/types"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"lib/types"
+	"log"
+	"net/http"
 	"net/url"
+	"os"
+	"path"
 	"strings"
-	"path")
-
-
+)
 
 var server string
 var port string
@@ -27,13 +27,13 @@ func init() {
 		usageServer   = "server to connect to"
 
 		defaultPort = "8080"
-		usagePort = "port to connect to"
+		usagePort   = "port to connect to"
 
 		defaultUser = ""
-		usageUser = "User to authenticate as"
+		usageUser   = "User to authenticate as"
 
 		defaultPassword = ""
-		usagePassword = "password to authenticate with"
+		usagePassword   = "password to authenticate with"
 	)
 	flag.StringVar(&server, "server", defaultServer, usageServer)
 	flag.StringVar(&server, "s", defaultServer, usageServer+" (shorthand)")
@@ -59,43 +59,40 @@ func main() {
 		key = login(username, password)
 	}
 
+	if len(args) == 0 {
+		printPlayList()
+	} else {
+		switch strings.ToLower(args[0]) {
+		case "playlist":
+			printPlayList()
+		case "submit":
+			if len(args) == 2 {
+				submit(args[1:], key)
+			} else {
+				fmt.Println("Submit <Filename>\n")
 
-    if len(args) == 0 {
-        printPlayList()
-    } else {
-        switch strings.ToLower(args[0]) {
-        case "playlist":
-                printPlayList()
-        case "submit":
-                if len(args) == 2 {
-                    submit(args[1:], key)
-                } else {
-                     fmt.Println("Submit <Filename>\n")
+			}
 
-                }
-
-        default:
-                fmt.Println("Unknown command: ", args[0])
-        }
-    }
-
+		default:
+			fmt.Println("Unknown command: ", args[0])
+		}
+	}
 
 }
 
 func login(username, password string) string {
 
-        login := types.Login{username, password}
-        res, err := json.Marshal(login)
-        if err != nil {
-            log.Fatal(err)
-        }
-        resp, err := http.PostForm(connectionString + "login", url.Values{"data": {string(res)}})
-        if err != nil {
-            log.Fatal(err)
-        }
+	login := types.Login{username, password}
+	res, err := json.Marshal(login)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := http.PostForm(connectionString+"login", url.Values{"data": {string(res)}})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-
-	r := new (types.LoginResp)
+	r := new(types.LoginResp)
 
 	decoder := json.NewDecoder(resp.Body)
 	e := decoder.Decode(r)
@@ -109,49 +106,48 @@ func login(username, password string) string {
 
 func submit(parms []string, key string) {
 
-        b, err := ioutil.ReadFile(parms[0])
-        if err != nil { panic(err) }
+	b, err := ioutil.ReadFile(parms[0])
+	if err != nil {
+		panic(err)
+	}
 
+	submit := types.NewSubmit(path.Base(parms[0]), b, key)
+	res, err := json.Marshal(submit)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := http.PostForm(connectionString+"submit", url.Values{"data": {string(res)}})
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        submit := types.NewSubmit(path.Base(parms[0]), b, key)
-        res, err := json.Marshal(submit)
-        if err != nil {
-            log.Fatal(err)
-        }
-        resp, err := http.PostForm(connectionString + "submit", url.Values{"data": {string(res)}})
-        if err != nil {
-            log.Fatal(err)
-        }
-
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            log.Fatal(err)
-        }
-
-        fmt.Printf(string(body))
+	fmt.Printf(string(body))
 }
 
 func printPlayList() {
-    resp, err := http.Get(connectionString + "playlist")
-    if err != nil {
-        log.Printf("error: %v\n", err)
-       os.Exit(-1)
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get(connectionString + "playlist")
+	if err != nil {
+		log.Printf("error: %v\n", err)
+		os.Exit(-1)
+	}
+	defer resp.Body.Close()
 
-    r := new ([]string)
+	r := new([]string)
 
-    decoder := json.NewDecoder(resp.Body)
-    e := decoder.Decode(r)
+	decoder := json.NewDecoder(resp.Body)
+	e := decoder.Decode(r)
 
-    if e != nil {
-	    log.Fatalf("Woops: %v", e)
-    }
+	if e != nil {
+		log.Fatalf("Woops: %v", e)
+	}
 
-    fmt.Printf("Playlist:\n")
-    for i, v := range(*r) {
-	    fmt.Printf("%d: %v\n",i,v)
-    }
+	fmt.Printf("Playlist:\n")
+	for i, v := range *r {
+		fmt.Printf("%d: %v\n", i, v)
+	}
 }
-
--- src/main.go
+++ src/main.go
@@ -1,29 +1,28 @@
 package main
 
-import "net/http"
-import "fmt"
-import "html"
-import "log"
-import "encoding/json"
-import ("lib/types"
-	"lib/usermanager"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"html"
 	"lib/playlist"
-    "flag"
-    "lib/xdgbase"
-    "time"
-    )
+	"lib/types"
+	"lib/usermanager"
+	"lib/xdgbase"
+	"log"
+	"net/http"
+	"time"
+)
 import "github.com/jmhodges/levigo"
-
 
 var db *levigo.DB
 var port string
 var dir string
 
-
 func init() {
 	const (
 		defaultPort = "8080"
-		usagePort = "port to connect to"
+		usagePort   = "port to connect to"
 
 		usageDIR = "dir for HDG configuration files"
 	)
@@ -34,9 +33,8 @@ func init() {
 	flag.StringVar(&port, "p", defaultPort, usagePort+" (shorthand)")
 
 	flag.StringVar(&dir, "dir", defaultDIR, usageDIR)
-	flag.StringVar(&dir, "d", defaultDIR, usageDIR +" (shorthand)")
+	flag.StringVar(&dir, "d", defaultDIR, usageDIR+" (shorthand)")
 }
-
 
 func main() {
 
@@ -50,7 +48,6 @@ func main() {
 	loginReq := make(chan usermanager.LoginMsg)
 	keyCheck := make(chan usermanager.KeyCheckMsg)
 
-
 	http.HandleFunc("/login", mklogin(loginReq))
 	http.HandleFunc("/playlist", mkplaylist(playlistReq))
 	http.HandleFunc("/submit", mksubmit(playlistAdd, keyCheck))
@@ -59,74 +56,71 @@ func main() {
 	})
 
 	opts := levigo.NewOptions()
-	opts.SetCache(levigo.NewLRUCache(3<<30))
+	opts.SetCache(levigo.NewLRUCache(3 << 30))
 	opts.SetCreateIfMissing(true)
-	db, err = levigo.Open(dir + "/db", opts)
+	db, err = levigo.Open(dir+"/db", opts)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	go playlist.PlaylistManager(db, playlistReq, playlistAdd, dir)
 	go usermanager.UserManger(loginReq, keyCheck)
-	go log.Fatal(http.ListenAndServe(":" + port, nil))
+	go log.Fatal(http.ListenAndServe(":"+port, nil))
 
 	time.Sleep(time.Second)
 	log.Println("All done setting up.")
 }
 
-
 func mksubmit(out chan types.Submit, keyCheck chan usermanager.KeyCheckMsg) func(http.ResponseWriter, *http.Request) {
-        return func(w http.ResponseWriter, r *http.Request) {
-            if r.Method == "POST" {
-                log.Printf("New submit attempt")
-                r.ParseForm()
-                var jsonLogin = r.FormValue("data")
-                submit := new(types.Submit)
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			log.Printf("New submit attempt")
+			r.ParseForm()
+			var jsonLogin = r.FormValue("data")
+			submit := new(types.Submit)
 
+			var e = json.Unmarshal([]byte(jsonLogin), submit)
+			if e == nil {
+				log.Printf("Submit Request from: %v", submit.Key)
 
-                var e = json.Unmarshal([]byte(jsonLogin), submit)
-                if e == nil {
-                    log.Printf("Submit Request from: %v", submit.Key)
-
-			permok, userok := checkPerms(keyCheck, submit.Key, usermanager.PermSubmit)
-			if (!userok) {
-				log.Println("Submit failed from unkown user key: \"", submit.Key, "\"")
-				w.WriteHeader(http.StatusForbidden);
-				return
-			}
-			if (!permok) {
-				log.Println("Submit failed userkey \"", submit.Key, "\" has invalide perms")
-				fmt.Fprintf(w,"{\"error\":\"Unknown User\"}")
-				return
-			}
-			    out <- *submit
+				permok, userok := checkPerms(keyCheck, submit.Key, usermanager.PermSubmit)
+				if !userok {
+					log.Println("Submit failed from unkown user key: \"", submit.Key, "\"")
+					w.WriteHeader(http.StatusForbidden)
+					return
+				}
+				if !permok {
+					log.Println("Submit failed userkey \"", submit.Key, "\" has invalide perms")
+					fmt.Fprintf(w, "{\"error\":\"Unknown User\"}")
+					return
+				}
+				out <- *submit
 			} else {
-			    log.Printf("SubmitFailed %v \"%v\"\n", e, r.FormValue("data"))
+				log.Printf("SubmitFailed %v \"%v\"\n", e, r.FormValue("data"))
 			}
-            } else {
-                http.NotFound(w, r)
-            }
-        }
+		} else {
+			http.NotFound(w, r)
+		}
+	}
 }
 
-func checkPerms(keyCheck chan usermanager.KeyCheckMsg,key string, perms int) (bool, bool) {
+func checkPerms(keyCheck chan usermanager.KeyCheckMsg, key string, perms int) (bool, bool) {
 	resp := make(chan usermanager.KeyCheckResp)
 
 	keyCheck <- usermanager.KeyCheckMsg{key, resp}
-	r :=<-resp
+	r := <-resp
 
-	if (r.OK) {
+	if r.OK {
 		if (r.Permissions & perms) == perms {
 			return true, true
 		} else {
 			return false, true
 		}
 	} else {
-		return false, false;
+		return false, false
 	}
 
 }
-
 
 func mkplaylist(out chan playlist.PlaylistReq) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -136,7 +130,7 @@ func mkplaylist(out chan playlist.PlaylistReq) func(http.ResponseWriter, *http.R
 		playlist := <-c
 		res, err := json.Marshal(playlist)
 		if err != nil {
-			log.Panic (err)
+			log.Panic(err)
 			//XXX: handel err
 		}
 		log.Printf("send user playist %v\n", string(res))
@@ -145,7 +139,7 @@ func mkplaylist(out chan playlist.PlaylistReq) func(http.ResponseWriter, *http.R
 }
 
 func mklogin(out chan usermanager.LoginMsg) func(http.ResponseWriter, *http.Request) {
-	return func (w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			log.Printf("New Login attempt")
 			r.ParseForm()
@@ -153,7 +147,7 @@ func mklogin(out chan usermanager.LoginMsg) func(http.ResponseWriter, *http.Requ
 			login := new(types.Login)
 			var e = json.Unmarshal([]byte(jsonLogin), login)
 			if e == nil {
-				resp := make (chan usermanager.LoginResp)
+				resp := make(chan usermanager.LoginResp)
 				out <- usermanager.LoginMsg{*login, resp}
 				r := <-resp
 
